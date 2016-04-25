b0e6108b138484e153def68f0749c1aa5e4b6075
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-04-25 12:19:43 -0700

    finished todos, but not up and running

---
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -0,0 +1,23 @@
+(function(module) {
+  var articlesController = {};
+
+  // DONE: Create the `articles` table when the controller first loads,
+  //  with the code that used to be in index.html:
+Article.createTable();
+
+  articlesController.index = function() {
+    // DONE: Complete this function that kicks off the fetching and rendering
+    //  of articles, using the same
+    //  code that used to be in index.html:
+    // initialize index page, articleView maybe? ER.
+    Article.fetchAll(articleView.initIndexPage);
+
+    // DONE: But wait! There's more: Also be sure to hide all the main section
+    // elements, and reveal the articles section:
+    $('main section').hide();
+    $('#articles').show();
+    //now the route controls this, not a jQuery event!!! We're querrying the DOM over and over listening for events. We're also spoofed the browser into thinking we're on another page to save the history url.
+  };
+
+  module.articlesController = articlesController;
+})(window);
---
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -0,0 +1,7 @@
+// DONE: Configure our two routes for this app with page.js, by registering
+//  each URL your app can handle, linked to a single controller function to handle it:
+page('/', articlesController.index);
+page('/about', aboutController.index);
+
+// DONE: What function do we call to activate page.js? Fire it off now, to execute it:
+page.start();
